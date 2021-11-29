6e6dc98eb41a30bfbc135a7721cdcae2931ac54b
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-29 16:47:27 +0100

    bumping cart button when it's changed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,27 @@
-import {useContext} from "react";
-import CartContext  from "../../context/cart-context";
-import CartIcon     from "../Cart/CartIcon";
-import styles       from "./HeaderCartButton.module.css";
+import {useContext, useEffect, useState} from "react";
+import CartContext                       from "../../context/cart-context";
+import CartIcon                          from "../Cart/CartIcon";
+import styles                            from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = () => {
+  const [buttonHighlighted, setButtonHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
+
   const numberOfCartItems = cartContext.cartItems
       .reduce((curNumber, item) => curNumber + item.amount, 0);
 
-  return <button className={styles.button} onClick={cartContext.showCart}>
+  const btnClasses = `${styles.button} ${buttonHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (cartContext.cartItems.length > 0) {
+      setButtonHighlighted(true);
+    }
+    const timer = setTimeout(() => setButtonHighlighted(false), 300);
+
+    return () => clearTimeout(timer);
+  }, [cartContext.cartItems]);
+
+  return <button className={btnClasses} onClick={cartContext.showCart}>
     <span className={styles.icon}><CartIcon/></span>
     <span>Your Cart</span>
     <span className={styles.badge}>{numberOfCartItems}</span>
